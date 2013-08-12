ffad5f2745f97c88961100a3f3bb7c559848fe78
yangacer <yangacer@gmail.com>
2013-08-12 08:41:18 +0800

    Add comments to main header.

--- discov.hpp
+++ discov.hpp
@@ -3,6 +3,12 @@
 
 #include <sstream>
 
+/** Discover services via mDNS.
+ * @param sstream Results formated as JSON.
+ * @param type Service type such as '_http._tcp'.
+ * @param wait_time Timeout when waitting for mDNS response.
+ * @param prefix Prefix for saving know_host.
+ */
 void discov(std::stringstream &sstream, char const *type, int wait_time = 2, char const *prefix = ".");
 
 #endif
